c35547525aa123e15f877534d62040a06b599e84
munir-terravesta <munir@terravesta.com>
2023-11-18 19:12:00 +0000

    113_Validating passwords

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -10,12 +10,13 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {
-    // console.log(this.modal.visible);
+  constructor(
+    public modal: ModalService, 
+    public el: ElementRef) {
   }
 
   ngOnInit(): void {
-    
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
